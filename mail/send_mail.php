6eee5c4681449ee07b0bce4bfe24beb952f5d021
Anton Bukhtiyarov <AntonBukhtiyarov@Antons-MacBook-Pro.local>
2016-03-21 19:50:05 +0700

    email form

---
+++ mail/send_mail.php
@@ -0,0 +1,44 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+	
+// Create the email and send the message
+$to = 'buhas@mail.ru'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+
+if ($curl = curl_init()) {
+	$post = [
+		'from'    => $email_address,
+		'to'      => $to,
+		'subject' => $email_subject,
+		'text'    => $email_body
+	];
+	curl_setopt($curl, CURLOPT_USERPWD, "api:key-91e878ac7382705078e0926b4c38e286");  
+	curl_setopt($curl, CURLOPT_URL, "https://api.mailgun.net/v3/mg.antonbukhtiyarov.ru/messages");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+	
+	curl_exec($curl);
+	curl_close();
+	
+	return true;
+}
+
+?>
