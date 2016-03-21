21afe7e7f7991938ba8b8d9f74826737acb18568
Anton Bukhtiyarov <AntonBukhtiyarov@Antons-MacBook-Pro.local>
2016-03-21 20:09:16 +0700

    email form

--- mail/send_mail.php
+++ mail/send_mail.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL); 
 ini_set("display_errors", 1); 
 
-echo "name: $_POST['name']\n";
+echo "name: "; echo $_POST['name']";
 echo "email: $_POST['email']\n";
 echo "phone: $_POST['phone']\n";
 echo "message: $_POST['message']\n";
