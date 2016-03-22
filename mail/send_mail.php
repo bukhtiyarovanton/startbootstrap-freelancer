f69534735b25d57ceef92f8a4d072ec2c72e8bd7
Anton Bukhtiyarov <AntonBukhtiyarov@Antons-MacBook-Pro.local>
2016-03-22 09:37:27 +0700

    added new fields to request form

--- mail/send_mail.php
+++ mail/send_mail.php
@@ -17,7 +17,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'bukhtiyarov.anton@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'bukhtiyarov.anton@gmail.com';//,anton.gomzyakov2015@yandex.ru'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 
